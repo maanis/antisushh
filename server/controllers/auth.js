9913c43b80b483a5dabb8a5ef7cfd910732bf465
Manish Jha <manishjha962@gmail.com>
2025-02-24 19:09:05 +0530

    add bcrypt for password hashing and implement user login functionality

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,5 @@
 const userModel = require("../model/userModel");
+const bcrypt = require('bcryptjs');
 
 const register = async function (req, res) {
     const { name, username, password } = req.body;
@@ -10,17 +11,39 @@ const register = async function (req, res) {
             res.send('user already exist');
             return;
         }
-
+        const hassPass = await bcrypt.hash(password, 10);
         const user = await userModel.create({
             name,
             username,
-            password,
+            password: hassPass
         });
         res.send(user);
     } catch (error) {
         res.send('internal server error');
+        res.send(error);
     }
+}
 
+const login = async function (req, res) {
+    const { username, password } = req.body;
+    try {
+
+        const exist = await userModel.findOne({ username });
+
+        if (!exist) {
+            res.send('user not exist');
+            return;
+        }
+        const isMatch = await bcrypt.compare(password, exist.password);
+        if (!isMatch) {
+            res.send('Wrong credentials');
+            return;
+        }
+        res.send(user);
+    } catch (error) {
+        res.send('internal server error');
+        res.send(error);
+    }
 }
 
 module.exports = { register };
