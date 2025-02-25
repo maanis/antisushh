5035e9a5b1725d3d16bd6f10483c1d7e837e573b
Manish Jha <manishjha962@gmail.com>
2025-02-25 15:31:15 +0530

    add JWT authentication and user session management

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,14 +1,18 @@
 const userModel = require("../model/userModel");
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const register = async function (req, res) {
     const { name, username, password } = req.body;
     try {
-
+        if (!name || !username || !password) {
+            res.status(400).json({ message: 'Please fill all the fields', success: false });
+            return;
+        }
         const exist = await userModel.findOne({ username });
 
         if (exist) {
-            res.send('user already exist');
+            res.status(400).json({ message: 'user already exist', success: false });
             return;
         }
         const hassPass = await bcrypt.hash(password, 10);
@@ -17,35 +21,53 @@ const register = async function (req, res) {
             username,
             password: hassPass
         });
-        res.send(user);
+        res.status(200).json({ message: 'user signed up', success: true });
     } catch (error) {
-        res.send('internal server error');
-        res.send(error);
+        res.status(500).json({ message: 'Internal server error', success: false });
     }
 }
 
 const login = async function (req, res) {
     const { username, password } = req.body;
     try {
-
-        const exist = await userModel.findOne({ username });
+        if (!username || !password) {
+            res.status(400).json({ message: 'Please fill all the fields', success: false });
+            return;
+        }
+        const exist = await userModel.findOne({ username }).select('-password');
 
         if (!exist) {
-            res.send('user not exist');
+            res.status(400).json({ message: 'User not exist', success: false });
             return;
         }
         const isMatch = await bcrypt.compare(password, exist.password);
         if (!isMatch) {
-            res.send('Wrong credentials');
+            res.status(400).json({ message: 'Wrong credentials', success: false });
             return;
         }
-        res.send(exist);
+        const token = jwt.sign({ userId: exist._id }, process.env.JWT_SECRET);
+        res.cookie('token', token)
+        res.status(200).json({ message: 'Signed in', success: true, exist });
+
     } catch (error) {
-        res.send('internal server error');
-        res.send(error);
+        res.status(500).json({ message: 'Internal server error', success: false });
+    }
+}
+
+const logout = async function (req, res) {
+    res.clearCookie('token');
+    res.status(200).json({ message: 'Logged out', success: true });
+}
+
+
+const followOrUnfollow = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error', success: false });
     }
 }
 
 
 
-module.exports = { register, login };
+module.exports = { register, login, logout };
---
+++ server/middlewares/isAuthenticated.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const isAuthenticated = async function (req, res, next) {
+    try {
+        const token = req.cookies.token;
+        if (!token) {
+            res.status(401).json({ message: 'Unauthorized', success: false });
+            return;
+        }
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        if (!decode) {
+            res.status(401).json({ message: 'Unauthorized', success: false });
+            return;
+        }
+        req.id = decode.userId;
+        next();
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error', success: false });
+    }
+}
