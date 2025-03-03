0621e38313baa29e68e66abbcda0e6e17db697cf
Manish Jha <manishjha962@gmail.com>
2025-03-03 20:27:51 +0530

    feat: implement create post functionality in Sidebar component with image upload and loading state

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { Cross, CrossIcon, Globe, HeartIcon, Home, LogOut, LucideCross, MessageCircle, PlusSquare, Search, X } from 'lucide-react'
+import { Cross, CrossIcon, Globe, HeartIcon, Home, Loader2, LogOut, LucideCross, MessageCircle, PlusSquare, Search, X } from 'lucide-react'
 import React, { useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
@@ -21,9 +21,12 @@ import { Button } from './ui/button'
 
 const Sidebar = () => {
     const navigate = useNavigate('/')
+    const [loading, setloading] = useState(false);
     const imgRef = useRef('')
     const [createDialog, setcreateDialog] = useState(false)
     const [preview, setpreview] = useState(false)
+    const [caption, setcaption] = useState('')
+    const [image, setimage] = useState('')
     const data = [
         { icon: <Home size={'20px'} />, text: 'home' },
         { icon: <Search size={'20px'} />, text: 'search' },
@@ -54,9 +57,38 @@ const Sidebar = () => {
     const handleImageUpload = () => {
         const file = imgRef.current.files[0]
         if (file) {
+            setimage(file)
             const url = fileToUrl(file);
             console.log("After:", url); // Logs the temporary URL
             setpreview(url);
+        }
+    }
+
+    const handleCreatePost = async () => {
+        try {
+            setloading(true)
+            const formData = new FormData();
+            formData.append('caption', caption);
+            formData.append('image', image);
+            const data = await fetch('http://localhost:3000/post/create', {
+                credentials: 'include',
+                method: 'POST',
+                body: formData,
+            })
+            const res = await data.json();
+            if (res.success) {
+                toast.success(res.message)
+                setcreateDialog(false)
+                setcaption('')
+                setpreview(null)
+                setimage(null)
+            } else {
+                toast.error(res.message)
+            }
+        } catch (error) {
+            toast.error('Something went wrong')
+        } finally {
+            setloading(false)
         }
     }
     return (
@@ -94,10 +126,19 @@ const Sidebar = () => {
                     <DialogContent onInteractOutside={() => setcreateDialog(false)} className='w-[90rem] bg-zinc-300 rounded-lg outline-none border-none'>
                         <DialogTitle className='hidden'>title</DialogTitle>
                         <div className='text-neutral-900  text-center text-xl font-semibold'>Create a Post</div>
-                        <textarea className='rounded-md max-h-20 outline-none text-black min-h-12 px-3 py-2' placeholder='Enter a caption...'></textarea>
+                        <textarea value={caption} onChange={(e) => setcaption(e.target.value)} className='rounded-md max-h-20 outline-none text-black min-h-12 px-3 py-2' placeholder='Enter a caption...'></textarea>
                         <input onChange={handleImageUpload} ref={imgRef} type="file" className='hidden' />
-                        {preview && <div className='w-full relative flex items-center justify-center'><img src={preview} className='rounded-md max-h-[45vh]' /><X className='absolute top-0 p-0 right-0 cursor-pointer text-red-500' onClick={() => setpreview(null)} /></div>}
-                        <button onClick={() => imgRef.current.click()} className='bg-blue-600 inline-block w-fit px-3 py-1 rounded-md text-white font-semibold hover:bg-blue-700 transition-all'>Upload an image</button>
+                        {preview && <div className='w-full relative flex items-center justify-center'><img src={preview} className='rounded-md max-h-[45vh] w-full object-cover' /><X className='absolute top-0 p-0 right-0 cursor-pointer text-red-500' onClick={() => setpreview(null)} /></div>}
+                        <button onClick={() => imgRef.current.click()} className='bg-blue-600 shadow-lg inline-block w-fit px-3 py-1 rounded-md text-white font-semibold hover:bg-blue-700 transition-all'>Upload an image</button>
+
+                        <button
+                            onClick={handleCreatePost}
+                            disabled={!caption || !preview}
+                            type="submit"
+                            className="w-full bg-gradient-to-r from-neutral-800 to-neutral-900 transition-colors text-white rounded-lg p-3 mt-6 font-medium hover:from-neutral-500 hover:to-neutral-600 focus:outline-none focus:ring-2 focus:ring-neutral-400 focus:ring-offset-2 focus:ring-offset-purple-900"
+                        >
+                            {loading ? (<h2 className='flex w-full justify-center gap-1'><Loader2 className='animate-spin font-bold' /><span>Please wait...</span></h2>) : 'Post'}
+                        </button>
                     </DialogContent>
                 </Dialog>
 
