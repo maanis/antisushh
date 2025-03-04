d46b96dd4c5dc07a7044af8b1a5a61fa9fb7e01d
Manish Jha <manishjha962@gmail.com>
2025-03-04 22:12:08 +0530

    feat: add timestamp formatting to Post component and enable automatic timestamp handling in post model

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -2,14 +2,23 @@ import { Bookmark, Ellipsis, Heart, MessageCircle, Send } from 'lucide-react'
 import React, { useState } from 'react'
 import CommentDialogBox from './CommentDialogBox'
 import { Dialog, DialogContent, DialogTitle } from './ui/dialog'
+import { userDefaultPfp } from '@/utils/constant'
+import { useSelector } from 'react-redux'
 
 const Post = ({ posts }) => {
     const { caption, comments, image, likes, user } = posts
+    const createdAt = new Date(posts.createdAt).toLocaleString();
+    const currentUser = useSelector(state => state.userInfo.user)
+    console.log(currentUser)
+    const date = new Date(createdAt);
+    const formattedTime = `${date.getHours()}:${date.getMinutes()}, ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+
+    console.log(formattedTime);
     // console.log(posts)
     // console.log(caption)
     const [open, setopen] = useState(false)
     const [ismenuopen, setismenuopen] = useState(false)
-    const data = ['unfollow', 'add to favourites', 'go to post', 'copy link', 'about this account', 'cancel']
+    const data = ['unfollow', 'delete', 'add to favourites', 'go to post', 'copy link', 'about this account', 'cancel']
     const handleMenuClick = (e) => {
         if (e === 'cancel') {
             setismenuopen(false)
@@ -19,14 +28,14 @@ const Post = ({ posts }) => {
         <div>
             <div className="post flex flex-col w-[60%] px-1 mx-auto mb-2 mt-4">
                 <div className="box-a flex items-center gap-2">
-                    <img src="https://images.unsplash.com/photo-1628157588553-5eeea00af15c?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    <img src={user.pfp ? user.pfp : userDefaultPfp}
                         className="w-10 h-10 object-cover object-top rounded-full" alt="" />
-                    <div className="dets">
+                    <div className="dets ml-1">
                         <h3 className="text-[16px]">
                             {user.username}
                         </h3>
-                        <p className="text-[12px] text-zinc-300">
-                            create
+                        <p className="text-[12px] text-zinc-500">
+                            {formattedTime}
                         </p>
                     </div>
                     <div className="ml-auto relative dot cursor-pointer">
@@ -35,6 +44,10 @@ const Post = ({ posts }) => {
                 </div>
                 <div className="box-b w-full border mt-2 flex justify-center border-zinc-800  ">
                     <img src={image} alt="" />
+                </div>
+                <div className="flex items-center mt-2">
+                    <h2 className='font-semibold'>{user.username}</h2>
+                    <p className='ml-2 text-zinc-300 font-light'>{caption}</p>
                 </div>
                 <div className="flex py-3 gap-3 ">
                     <Heart size={'20px'} className='cursor-pointer' />
@@ -52,7 +65,7 @@ const Post = ({ posts }) => {
                         <div className="w-full overflow-hidden rounded-lg bg-neutral-900">
                             <div className="flex flex-col">
                                 {data.map((e, i) => {
-                                    return <button key={i} onClick={() => handleMenuClick(e)} className={`w-full py-4 px-6 text-center focus:outline-none border-b border-neutral-800 transition-colors ${i === 0 ? 'text-red-500' : 'text-white'} capitalize hover:bg-neutral-800`}>
+                                    return <button key={i} onClick={() => handleMenuClick(e)} className={`w-full py-4 px-6 text-center focus:outline-none border-b border-neutral-800 transition-colors ${i === 0 || i === 1 ? 'text-red-500' : 'text-white'} capitalize hover:bg-neutral-800`}>
                                         {e}
                                     </button>
                                 })}
--- server/model/postModel.js
+++ server/model/postModel.js
@@ -6,7 +6,7 @@ const postSchema = mongoose.Schema({
     image: { type: String, required: true },
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
     comments: [{ user: { type: mongoose.Schema.Types.ObjectId, ref: 'user' }, text: { type: String, require: true } }]
-})
+}, { timestamps: true });
 
 const postModel = mongoose.model('post', postSchema);
 
