1b8ff7310eb2ca43ffde4e9c9fce3224f6dcafc6
maanis <manishjha962@gmail.com>
2025-03-30 16:31:06 +0530

    feat: integrate Socket.IO for user connection and online user tracking in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Register from './components/Register'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import Feed from './components/Feed'
@@ -8,10 +8,33 @@ import ProfilePage from './components/ProfilePage'
 import UpdateProfile from './components/UpdateProfile'
 import ProtectedUpdateProfile from './utils/ProtectedUpdateProfile'
 import ChatSection from './components/ChatSection'
+import { io } from 'socket.io-client'
+import { useSelector } from 'react-redux'
 
 const App = () => {
   const location = useLocation();
   const showSidebar = location.pathname !== "/";
+  const { user } = useSelector(store => store.userInfo)
+  useEffect(() => {
+    if (user) {
+      const socket = new io('http://localhost:3000', {
+        query: { userId: user._id },
+        transports: ['websocket'],
+      });
+      console.log('connected to socket:', socket);
+
+      socket.on('getOnlineUsers', (users) => {
+        console.log('Online users:', users);
+      })
+      return () => {
+        socket.disconnect();
+        console.log('Socket disconnected on cleanup:', socket.id);
+        socket.on('getOnlineUsers', (users) => {
+          console.log('Online users:', users);
+        })
+      }
+    }
+  }, [])
   return (
     <>
       <div className='h-screen flex bg-zinc-950 w-full'>
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,29 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // Uses localStorage by default
+import storage from 'redux-persist/lib/storage';
 import userReducer from './userSlice';
 import postReducer from './postSlice';
 
-// Persist config for user state (stores user info persistently)
 const userPersistConfig = {
     key: 'user',
     storage,
 };
 
-// Apply persistReducer only to user data
 const persistedUserReducer = persistReducer(userPersistConfig, userReducer);
 
 const store = configureStore({
     reducer: {
-        userInfo: persistedUserReducer, // Persist user state
-        posts: postReducer, // Do NOT persist posts to avoid storage issues
+        userInfo: persistedUserReducer,
+        posts: postReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
-            serializableCheck: false, // Disables warnings related to non-serializable data
+            serializableCheck: false,
         }),
 });
 
-// Persistor for Redux Persist (only for user info)
 export const persistor = persistStore(store);
 export default store;
